edcbbbb820cedc388e87b2866b8c371088e09363
Chris Chavez <chrischavezdataengineer@gmail.com>
2021-07-05 10:27:46 -0400

    VS CODE previously untracted commits

---
+++ VS CODE TUTORIAL/Review_ValidateList.py
@@ -0,0 +1,7 @@
+% PROJECT_10 project_10 is the driver function for the program.
+%
+%	Name: Chris Chavez
+%	Date: 06 JULY 2021
+%	Class: CMPSC 132
+%   Description: Implement Monte Carlo Method to Integrate a function.
+%
